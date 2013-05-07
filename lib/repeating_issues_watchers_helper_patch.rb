e9affa833a2e0a89c21e83ecb19d6849ac6ceed0
rubynovich <roman@shipiev.me>
2013-05-07 16:19:03 +0400

    fix

--- lib/repeating_issues_watchers_helper_patch.rb
+++ lib/repeating_issues_watchers_helper_patch.rb
@@ -15,10 +15,10 @@ module RepeatingIssuesPlugin
         if (object.class == Issue)&&(user.allowed_to?({:controller => :repeating_issues, :action => :new}, nil, {:global => true}))
           [watcher_tag_without_repeating_issues(object, user, options),
           content_tag("span", :class => :repeating_issues) do
-            if object.repeating_issue.present?
-              link_to(t(:button_repeating_issues_edit), {:action => :edit, :controller => :repeating_issues, :id => object.repeating_issue.id}, {:class => "icon icon-duplicate"}) + "<br>".html_safe
+             "<br>".html_safe + if object.repeating_issue.present?
+              link_to(t(:button_repeating_issues_edit), {:action => :edit, :controller => :repeating_issues, :id => object.repeating_issue.id}, {:class => "icon icon-duplicate"})
             else
-              link_to(t(:button_repeating_issues_new), {:action => :new, :controller => :repeating_issues, :repeating_issue => {:issue_id => object.id}}, {:class => "icon icon-duplicate"}) + "<br>".html_safe
+              link_to(t(:button_repeating_issues_new), {:action => :new, :controller => :repeating_issues, :repeating_issue => {:issue_id => object.id}}, {:class => "icon icon-duplicate"})
             end
           end].join(" ").html_safe
         else
